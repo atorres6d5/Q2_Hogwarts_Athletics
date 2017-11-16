35c759e5a77b1fd4439688d68daef477fa5df51c
Aldo Torres <atorres6d5@gmail.com>
2017-11-16 11:33:08 -0800

    made create button work

--- src/schedule.js
+++ src/schedule.js
@@ -1,23 +1,42 @@
 let devPath = 'http://localhost:3000/hogwarts'
 
 let tableBody = document.querySelector('#allData')
+let newMatchButton = document.querySelector('#makeNewMatch')
 
-console.log(tableBody);
 
 let schedule = () =>{
+  console.log('???');
   axios.get(`${devPath}/Matches`)
   .then(result=>{
   tableBody.innerHTML = makeTableBody(result.data)
   })
 }
+schedule()
+let datetime = document.querySelector('#dateTime').value
+console.log(dateTime.value);
+
 
 let makeTableBody = (data)=>{
-  let stuff = ''
+  let html = ''
   data.forEach(row=>{
-    stuff += `<tr><th scope="row">${row.id}</th><td>${row.home_team}</td><td>${row.away_team}</td><td>${row.winner}</td></tr>`
+    html += `<tr><th scope="row">${row.id}</th><td>${row.home_team}</td><td>${row.away_team}</td><td>${row.winner}</td></tr>`
   })
   //console.log(stuff);
-  return stuff
+  return html
 }
 
-schedule()
+newMatchButton.addEventListener('click', ()=>{
+
+  let home = document.querySelector('.homeTeam').value
+  let away = document.querySelector('.awayTeam').value
+  let datetime = document.querySelector('#dateTime').value
+  let input = {
+    home_team:home,
+    away_team:away,
+    match_date: dateTime.value
+  }
+
+  axios.post(`${devPath}/matches`, input)
+  .then(result=> schedule())
+
+})
