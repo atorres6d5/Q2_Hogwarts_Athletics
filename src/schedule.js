f58daa5143dc476cf2724f1ea42466c4f52c7191
Aldo Torres <atorres6d5@gmail.com>
2017-11-16 17:31:37 -0800

    mvp

--- src/schedule.js
+++ src/schedule.js
@@ -3,23 +3,41 @@ let devPath = 'http://localhost:3000/hogwarts'
 let tableBody = document.querySelector('#allData')
 let newMatchButton = document.querySelector('#makeNewMatch')
 let delButton = document.querySelector('#delButton')
-
+let winButt
+let winVal
 let schedule = () =>{
   console.log('scheule is up to date!');
   axios.get(`${devPath}/Matches`)
   .then(result=>{
-  tableBody.innerHTML = makeTableBody(result.data)
+    tableBody.innerHTML = makeTableBody(result.data)
+    winButt = document.querySelectorAll('.winner')
+    //winVal = document.querySelector('#winVal')
+    console.log(winButt)
+    winButt.forEach(button=>{button.addEventListener('click', (event)=>{
+      let id= event.target.id
+      winVal = document.querySelector(`#winVal${id}`)
+      console.log(id);
+      axios.patch(`${devPath}/matches/${id}`, {winner:winVal.value}).then(result=>{
+        schedule()
+      })
+    })})
   })
 }
 schedule()
 let datetime = document.querySelector('#dateTime').value
-console.log(dateTime.value);
+
 
 
 let makeTableBody = (data)=>{
+  //console.log(data)
   let html = ''
   data.forEach(row=>{
-    html += `<tr><th scope="row">${row.id}</th><td>${row.home_team}</td><td>${row.away_team}</td><td>${row.match_date}</td><td>${row.winner}</td></tr>`
+    if(row.winner===null){
+      row.winner="TBD"
+      html+= `<tr><th scope="row">${row.id}</th><td>${row.home_team}</td><td>${row.away_team}</td><td>${row.match_date}</td><td>${row.winner}</td><td><select id="winVal${row.id}"><option value=${row.homeTeamId}>${row.home_team}</option><option value=${row.awayTeamId}>${row.away_team}</option></select><br><button type="button" name="updateWinner" class="winner" id="${row.id}">Input Winner</button></td></tr>`
+    }
+    else{
+      html += `<tr><th scope="row">${row.id}</th><td>${row.home_team}</td><td>${row.away_team}</td><td>${row.match_date}</td><td>${row.winner}</td><td></td></tr>`}
   })
   //console.log(stuff);
   return html
