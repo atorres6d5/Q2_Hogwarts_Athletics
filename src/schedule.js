997dcbac6be92788fe675a0bd841b57d40ef96fa
Wes Reid <bwreid@gmail.com>
2017-12-08 12:17:53 -0800

    Update schedule.js

--- src/schedule.js
+++ src/schedule.js
@@ -1,66 +1,72 @@
-let devPath = 'http://localhost:3000/hogwarts'
+let devPath = 'http://localhost:3000/hogwarts' // This will not work in production
 
 let tableBody = document.querySelector('#allData')
 let newMatchButton = document.querySelector('#makeNewMatch')
 let delButton = document.querySelector('#delButton')
 let winButt
 let winVal
-let schedule = () =>{
-  console.log('scheule is up to date!');
+
+// In general your code spacing is all over the place. It makes it very
+// difficult to read what's going on. you will _need_ to get better at
+// this (or use some tooling!) before you graduate. You do not need
+// to write code exactly like below but it should all be consistent!
+
+// remove dead code and console.log statements!
+let schedule = () => {
   axios.get(`${devPath}/Matches`)
-  .then(result=>{
+  .then(result => {
     tableBody.innerHTML = makeTableBody(result.data)
     winButt = document.querySelectorAll('.winner')
-    //winVal = document.querySelector('#winVal')
-    console.log(winButt)
-    winButt.forEach(button=>{button.addEventListener('click', (event)=>{
-      let id= event.target.id
-      winVal = document.querySelector(`#winVal${id}`)
-      console.log(id);
-      axios.patch(`${devPath}/matches/${id}`, {winner:winVal.value}).then(result=>{
-        schedule()
+    winButt.forEach(button => {
+      button.addEventListener('click', (event) => {
+        let id = event.target.id
+        winVal = document.querySelector(`#winVal${id}`)
+        axios.patch(`${devPath}/matches/${id}`, {winner:winVal.value}).then(result => {
+          schedule()
+        })
       })
-    })})
+    })
   })
 }
+
 schedule()
+
+// this is only going to happen once, when your page loads.
+// it doesn't look like you are using this, though?
 let datetime = document.querySelector('#dateTime').value
 
-
-
-let makeTableBody = (data)=>{
-  //console.log(data)
+let makeTableBody = (data) => {
   let html = ''
-  data.forEach(row=>{
-    if(row.winner===null){
-      row.winner="TBD"
-      html+= `<tr><th scope="row">${row.id}</th><td>${row.home_team}</td><td>${row.away_team}</td><td>${row.match_date}</td><td>${row.winner}</td><td><select id="winVal${row.id}"><option value=${row.homeTeamId}>${row.home_team}</option><option value=${row.awayTeamId}>${row.away_team}</option></select><br><button type="button" name="updateWinner" class="winner" id="${row.id}">Input Winner</button></td></tr>`
+  data.forEach(row => {
+    if (row.winner === null) {
+      row.winner = "TBD"
+      // these long strings can and probably should be in their own file
+      html += `<tr><th scope="row">${row.id}</th><td>${row.home_team}</td><td>${row.away_team}</td><td>${row.match_date}</td><td>${row.winner}</td><td><select id="winVal${row.id}"><option value=${row.homeTeamId}>${row.home_team}</option><option value=${row.awayTeamId}>${row.away_team}</option></select><br><button type="button" name="updateWinner" class="winner" id="${row.id}">Input Winner</button></td></tr>`
+    } else {
+      html += `<tr><th scope="row">${row.id}</th><td>${row.home_team}</td><td>${row.away_team}</td><td>${row.match_date}</td><td>${row.winner}</td><td></td></tr>`
     }
-    else{
-      html += `<tr><th scope="row">${row.id}</th><td>${row.home_team}</td><td>${row.away_team}</td><td>${row.match_date}</td><td>${row.winner}</td><td></td></tr>`}
   })
-  //console.log(stuff);
+  
   return html
 }
 
-newMatchButton.addEventListener('click', ()=>{
-
+newMatchButton.addEventListener('click', () => {
   let home = document.querySelector('.homeTeam').value
   let away = document.querySelector('.awayTeam').value
   let datetime = document.querySelector('#dateTime').value
+
   let input = {
-    home_team:home,
-    away_team:away,
+    home_team: home,
+    away_team: away,
     match_date: dateTime.value
   }
 
   axios.post(`${devPath}/matches`, input)
-  .then(result=> schedule())
-
+  .then(result => schedule())
 })
 
-delButton.addEventListener('click', ()=>{
-  let matchID= document.querySelector('#matchID').value
+delButton.addEventListener('click', () => {
+  let matchID = document.querySelector('#matchID').value
   axios.delete(`${devPath}/matches/${matchID}`)
-  .then(result=> schedule())
+  .then(result => schedule())
 })
